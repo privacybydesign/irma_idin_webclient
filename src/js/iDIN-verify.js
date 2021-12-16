a1fb785d36aa3a4d611f9a180675b0bc30e4591b
Ivar Derksen <ivar.derksen@sidn.nl>
2021-12-16 11:33:28 +0100

    Refactor: use single quotes consistently in JavaScript

--- src/js/iDIN-verify.js
+++ src/js/iDIN-verify.js
@@ -1,48 +1,48 @@
 getSetupFromJson(function() {
     var success_fun = function(data) {
-        $("#result_status").html(strings.verify_success);
-        $("#result_header").html(strings.verify_result);
-        var bd = jwt_decode(data).attributes[conf.idin_credential_id + ".dateofbirth"];
-        $("#token-content").html("<b>" + strings.verify_birthdate + "</b> " + bd);
+        $('#result_status').html(strings.verify_success);
+        $('#result_header').html(strings.verify_result);
+        var bd = jwt_decode(data).attributes[conf.idin_credential_id + '.dateofbirth'];
+        $('#token-content').html('<b>' + strings.verify_birthdate + '</b> ' + bd);
         //TODO: check for birthdate
     }
 
     var cancel_fun = function(data) {
-        $("#result_header").html(strings.verify_result);
-        $("#result_status").html(strings.verify_cancelled);
+        $('#result_header').html(strings.verify_result);
+        $('#result_status').html(strings.verify_cancelled);
     }
 
     var error_fun = function(data) {
-        console.log("Verification failed!");
-        console.log("Error data:", data);
-        $("#result_header").html(strings.verify_result);
-        $("#result_status").html(strings.verify_failed);
+        console.log('Verification failed!');
+        console.log('Error data:', data);
+        $('#result_header').html(strings.verify_result);
+        $('#result_status').html(strings.verify_failed);
     }
 
     function irma_session_failed (msg) {
-        if(msg === "Aborted") {
+        if(msg === 'Aborted') {
             cancel_fun(msg);
         } else {
             error_fun(msg);
         }
     }
 
-    $("#verify_idin_bd_btn").on("click", function() {
-        $("#result_header").text("");
-        $("#result_status").text("");
-        $("#token-content").text("");
+    $('#verify_idin_bd_btn').on('click', function() {
+        $('#result_header').text('');
+        $('#result_status').text('');
+        $('#token-content').text('');
         $.ajax({
-            type: "GET",
-            url: server + "/verify",
+            type: 'GET',
+            url: server + '/verify',
             success: function(data) {
                 irma.newPopup({
                     language: irma_server_conf.language,
                     session: {
                         url: irma_server_conf.server,
                         start: {
-                            method: "POST",
+                            method: 'POST',
                             headers: {
-                                "Content-Type": "text/plain",
+                                'Content-Type': 'text/plain',
                             },
                             body: data,
                         },
